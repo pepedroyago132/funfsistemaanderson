c75ece0920973f398d50ce3ef5b9235fa463a8e8
pepedroyago132 <pedroyago1324@gmail.com>
2025-04-11 14:45:39 -0300

    up

--- src/pages/Config/index.js
+++ src/pages/Config/index.js
@@ -116,7 +116,7 @@ const Configuracao = () => {
                     <SubTitle>Mensagem Escolhida: {userData.msgCadastro}</SubTitle>
 
 
-                    <TitleForm>Enviado para horário das medicações:</TitleForm>
+                    <TitleForm>Enviado :</TitleForm>
                     <TextField
                         id={`outlined-basic`}
                         label={`Mensagem de lembrete - Horários`}
